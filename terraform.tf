4d86c9790fb5f985d56bba40dc2e6a3a20f04ed2
ip999 <porteous@gmail.com>
2020-03-06 13:31:04 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 0.12"
   backend "remote" {
-    organization = "my-organization"
+    organization = "cn-dino"
 
     workspaces {
       name = "gke-tutorial"
